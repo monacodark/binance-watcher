01f668edd88333e5b3b37065391dd351372dc375
Roma Ivanov <r.ivanov@betaonline.ru>
2020-12-02 22:12:43 +0300

    vuetify configured

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib'
+
+Vue.use(Vuetify)
+
+export default new Vuetify({
+  theme: {
+    dark: true,
+    themes: {
+      dark: {
+        primary: '#F0B90B',
+        secondary: '#424242',
+        accent: '#82B1FF',
+        error: '#FF5252',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FFC107'
+      }
+    }
+  }
+})
