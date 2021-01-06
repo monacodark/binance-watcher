9791a36da5d2fbad938187dfd28e0d93bb35969e
monacodark <monacodark@gmail.com>
2021-01-06 08:32:57 +0300

    added electron

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,8 +1,24 @@
 <script>
-// @ is an alias to /src
+import TradingVue from 'trading-vue-js'
 
 export default {
   name: 'chart',
+  components: {
+    'trading-vue': TradingVue,
+  },
+  data() {
+    return {
+      chart: {
+        ohlcv: [
+          [1551128400000, 33, 37.1, 14, 14, 196],
+          [1551132000000, 13.7, 30, 6.6, 30, 206],
+          [1551135600000, 29.9, 35, 21.3, 21.8, 74],
+          [1551139200000, 21.7, 25.9, 18, 24, 140],
+          [1551142800000, 24.1, 24.1, 24, 24.1, 29],
+        ],
+      },
+    }
+  },
 }
 </script>
 
@@ -15,11 +31,7 @@ export default {
     <v-divider />
 
     <v-card-text>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Ea, sapiente. Iure delectus suscipit soluta natus omnis
-      sint tempore eveniet?
-      Non nostrum, ratione distinctio sunt accusamus neque
-      vel repellat ipsa placeat?
+      <trading-vue :data="chart" />
     </v-card-text>
 
     <v-divider />
